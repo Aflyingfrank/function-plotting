5aeebb01735d674146e43f980baeb111318d0fb3
Thalia Seale <tseale@RF62X74HQC-7.local>
2024-07-05 11:14:10 +0800

    Changed the function plotted in the code

--- function plotting.R
+++ function plotting.R
@@ -1,15 +1,17 @@
 #domain of functions
-x <- seq(-2*pi, 2*pi, 0.1)
+# x <- seq(-2*pi, 2*pi, 0.1)
+x <- 1:1000/1000
 
 #plot sin(x) in line
-plot (x, sin(x), type = "l")
+plot (x, dbeta(x,50,50), type = "l")
 
 #polot sin(x) in scattered dots
-plot (x, sin(x))
+plot (x, y)
 
 #combined function graphs
-plot(x, sin(x), main="Trigonometry Graphs", ylab="", type="l", col="blue")
+plot(x, y, main="Beta Distribution Graphs", ylab="", type="l", col="blue")
 
-lines(x,cos(x), col="red")
+lines(x,dbeta(x,10,5), col="red")
 
-legend("bottomleft", c("sin(x)","cos(x)"), fill=c("blue","red"))
+legend("bottomleft", c("Beta(50,50)","Beta(10,5)"), fill=c("blue","red"))
+
