84d053aecd1c30035d7b8da317ccc4957ae212ff
Aflyingfrank <frank_zhongsh@outlook.com>
2024-07-05 11:21:13 +0800

    Added beta distribution graphing

--- function plotting.R
+++ function plotting.R
@@ -8,8 +8,19 @@ plot (x, sin(x), type = "l")
 plot (x, sin(x))
 
 #combined function graphs
-plot(x, sin(x), main="Trigonometry Graphs", ylab="", type="l", col="blue")
+plot(x, sin(x), main="Trigonometry Graphs", ylab="y", type="l", col="blue")
 
 lines(x,cos(x), col="red")
 
-legend("bottomleft", c("sin(x)","cos(x)"), fill=c("blue","red"))
+legend("bottomleft", c("y=sin(x)","y=cos(x)"), fill=c("blue","red"))
+
+################################################################################################################################
+x <- 1:1000/1000
+
+#plot beta distribution of x in line
+plot (x, dbeta(x,50,50), type = "l", main="Beta Distribution Graphs", ylab="", col="blue")
+
+#combined function graphs
+lines(x,dbeta(x,10,5), col="red")
+
+legend("topleft", c("Beta(50,50)","Beta(10,5)"), fill=c("blue","red"))
